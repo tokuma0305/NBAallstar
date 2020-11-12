a0ef176c811eccdf3f00571d3a112f5b60ed2d27
“tokuma0305” <yoshinari.tokuma.infratop@gmail.com>
2020-11-12 13:35:08 +0900

    itiran

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'home/about'
   devise_for :users
   root to:"home#top"
+  resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
