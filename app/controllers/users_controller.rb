39f75df73233f59db110eb6e5fa287148cdb42c2
“tokuma0305” <yoshinari.tokuma.infratop@gmail.com>
2020-11-13 21:21:39 +0900

    alomost nothing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        @vote = Vote.new
+    end
+    private
+    def user_params
+        params.require(:users).permit(:name,:)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root to:"home#top"
   resources :users
   resources :players
+  resources :votes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
